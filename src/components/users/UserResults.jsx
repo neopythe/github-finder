274fc532fcd2203643a159933b1d716cf3654ce9
Evan Bay <neopythe@protonmail.ch>
2022-07-07 23:50:14 -0600

    display users

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 import Spinner from '../layout/Spinner'
+import UserItem from './UserItem'
 
 function UserResults() {
   const [users, setUsers] = useState([])
@@ -30,7 +31,7 @@ function UserResults() {
     return (
       <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
         {users.map((user) => (
-          <h3>{user.login}</h3>
+          <UserItem key={user.id} user={user} />
         ))}
       </div>
     )

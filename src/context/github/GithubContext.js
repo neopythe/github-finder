bd95cd0056a21ed0194bb24769e1defc5024940b
Evan Bay <neopythe@protonmail.ch>
2022-08-01 18:46:56 -0600

    wire up AlertContext and AlertReducer

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -32,7 +32,6 @@ export const GithubProvider = ({ children }) => {
         Authorization: `token ${GITHUB_TOKEN}`,
       },
     })
-    console.log(items)
     dispatch({
       type: 'GET_USERS',
       payload: items,
